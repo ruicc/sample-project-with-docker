640fe66614c3c182bb4a4467470cb75904e32396
Kousuke Ruichi <ruicc.rail@gmail.com>
2015-08-07 05:05:24 +0900

    Add xhprof and opcache to web

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
 
+# xhprof
+xhprof_enable();
+$foo = strlen('heyhey');
+$profile = xhprof_disable();
+
+echo '<pre>';
+    print_r($profile);
+
+    # curl
+    $ch = curl_init('http://httpbin.org/get');
+    curl_exec($ch);
+    curl_close($ch);
+echo '</pre>';
+
 phpinfo();
